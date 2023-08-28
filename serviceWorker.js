802223ea385a5ebbf6ec0379468c7b4620b91a30
tonnywilliam-svg <63226542+tonnywilliam-svg@users.noreply.github.com>
2023-08-28 16:13:25 -0700

    Create serviceWorker.js

---
+++ serviceWorker.js
@@ -0,0 +1,23 @@
+const staticDevCoffee = "hx-pwa-github-page"
+const assets = [
+  "/",
+  "/index.html",
+  "/app.js",
+  "/iced-coffee.jpg",
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
